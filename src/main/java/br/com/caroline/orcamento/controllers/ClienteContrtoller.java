3d3c45d9eb23c5a9c50426c38d96b9b74650d604
carlainha <carolradichi23@gmail.com>
2024-07-31 08:58:35 -0300

    Aula dia 31/07

---
+++ src/main/java/br/com/caroline/orcamento/controllers/ClienteContrtoller.java
@@ -0,0 +1,21 @@
+package br.com.caroline.orcamento.controllers;
+
+import br.com.caroline.orcamento.model.Cliente;
+import br.com.caroline.orcamento.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteContrtoller {
+    @Autowired
+    private ClienteService clienteService;
+    public ResponseEntity<Cliente> inserir (@RequestBody Cliente cliente){
+        Cliente clienteSalva = clienteService.salvar(cliente);
+        return ResponseEntity.status(HttpStatus.CREATED).body(clienteSalva);
+    }
+}
---
+++ src/main/java/br/com/caroline/orcamento/controllers/MunicipioController.java
@@ -0,0 +1,21 @@
+package br.com.caroline.orcamento.controllers;
+
+import br.com.caroline.orcamento.model.Municipio;
+import br.com.caroline.orcamento.services.MunicipioService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/municipios")
+public class MunicipioController {
+    @Autowired
+    private MunicipioService municipioService;
+    public ResponseEntity<Municipio> inserir (@RequestBody Municipio municipio){
+        Municipio municipoSalva = municipioService.salvar(municipio);
+        return ResponseEntity.status(HttpStatus.CREATED).body(municipoSalva);
+    }
+}
