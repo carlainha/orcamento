c7da4cb4f661d28606870e2c5749241df909c388
carlainha <carlainha>
2024-08-07 08:52:28 -0300

    Aula dia 07/08

--- src/main/java/br/com/caroline/orcamento/controllers/LancamentoController.java
+++ src/main/java/br/com/caroline/orcamento/controllers/LancamentoController.java
@@ -5,6 +5,7 @@ import br.com.caroline.orcamento.services.LancamentoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class LancamentoController {
     @Autowired
     private LancamentoService lancamentoService;
+
+    @PostMapping()
     public ResponseEntity<Lancamento> inserir (@RequestBody Lancamento lancamento){
         Lancamento lancamentoSalva = lancamentoService.salvar(lancamento);
         return ResponseEntity.status(HttpStatus.CREATED).body(lancamentoSalva);
--- src/main/java/br/com/caroline/orcamento/controllers/MunicipioController.java
+++ src/main/java/br/com/caroline/orcamento/controllers/MunicipioController.java
@@ -5,6 +5,7 @@ import br.com.caroline.orcamento.services.MunicipioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MunicipioController {
     @Autowired
     private MunicipioService municipioService;
+
+    @PostMapping()
     public ResponseEntity<Municipio> inserir (@RequestBody Municipio municipio){
         Municipio municipoSalva = municipioService.salvar(municipio);
         return ResponseEntity.status(HttpStatus.CREATED).body(municipoSalva);
